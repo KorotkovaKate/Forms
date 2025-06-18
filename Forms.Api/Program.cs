7b0cd036c821c075cbfed1f0e91b9313f22f9be1
KorotkovaKate <korotkovakate10@gmail.com>
2025-06-18 03:20:24 +0300

    Add DbContext to Program class

--- Forms.Api/Program.cs
+++ Forms.Api/Program.cs
@@ -1,6 +1,12 @@
+using Forms.DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.AddDbContext<FormDbContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString(nameof(FormDbContext)));
+});
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
 app.Run();
