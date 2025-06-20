c355e38d474ae42eaf54fa7dec35149b19aa7013
KorotkovaKate <korotkovakate10@gmail.com>
2025-06-20 22:33:12 +0300

    Edit interfaces of repositories(ILikeTemplate, IQuestionOptionRepository)

--- Forms.Core/Interfaces/IRepositories/ILikedTemplateRepository.cs
+++ Forms.Core/Interfaces/IRepositories/ILikedTemplateRepository.cs
@@ -5,6 +5,6 @@ namespace Forms.Core.Interfaces.IRepositories;
 public interface ILikedTemplateRepository
 {
     public Task<List<Template>> GetLikedTemplatesByUserId(uint userId);
-    public Task AddLikedTemplate(uint userId, uint templateId);
-    public Task RemoveLikedTemplate(uint userId, uint templateId);
+    public Task AddLikedTemplate(LikedTemplate likedTemplate);
+    public Task RemoveLikedTemplate(LikedTemplate likedTemplate);
 }
--- Forms.Core/Interfaces/IRepositories/IQuestionOptionRepository.cs
+++ Forms.Core/Interfaces/IRepositories/IQuestionOptionRepository.cs
@@ -5,4 +5,7 @@ namespace Forms.Core.Interfaces.IRepositories;
 public interface IQuestionOptionRepository
 {
     public Task<List<QuestionOption>> GetOptions(uint questionId);
+    public Task AddOption(QuestionOption questionOption);
+    public Task DeleteOption(QuestionOption questionOption);
+    public Task<QuestionOption?> GetOption(uint questionOptionId);
 }
