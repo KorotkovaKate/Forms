a9f829fd11318e4ee170656f4975f55c427a5572
KorotkovaKate <korotkovakate10@gmail.com>
2025-06-21 10:10:38 +0300

    Add user DTOs for autorization and registration

---
+++ Forms.Application/DTOs/AutorizationDto.cs
@@ -0,0 +1,9 @@
+using Forms.Core.Enums;
+
+namespace Forms.Application.DTOs;
+
+public class AutorizationDto
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ Forms.Application/DTOs/RegistrationDto.cs
@@ -0,0 +1,12 @@
+using Forms.Core.Enums;
+
+namespace Forms.Application.DTOs;
+
+public class RegistrationDto
+{
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    
+    public UserRole Role { get; set; } = UserRole.User;
+}
