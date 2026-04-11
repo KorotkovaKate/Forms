38494ce0a4c063b4cf392add7beb3035e957bd67
KorotkovaKate <korotkovakate10@gmail.com>
2026-04-11 20:13:47 +0300

    feat: connect DI files, ref services

--- Forms.Api/Program.cs
+++ Forms.Api/Program.cs
@@ -1,61 +1,28 @@
-using Forms.Application.Interfaces.ISecurity;
-using Forms.Application.Interfaces.IServices;
-using Forms.Application.JwtTokens;
-using Forms.Application.Services;
-using Forms.Core.Interfaces.IRepositories;
+using Forms.Application;
 using Forms.DAL;
-using Forms.DAL.Repositories;
-using Forms.DAL.Security;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
+var services = builder.Services;
 
-builder.Services.AddDbContext<FormDbContext>(options =>
-{
-    options.UseSqlServer(configuration.GetConnectionString(nameof(FormDbContext)));
-});
+services
+    .AddApplication(configuration)
+    .AddDal(configuration);
 
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<ITemplateRepository, TemplateRepository>();
-builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
-builder.Services.AddScoped<IQuestionOptionRepository, QuestionOptionRepository>();
-builder.Services.AddScoped<IFormRepository, FormRepository>();
-builder.Services.AddScoped<IAnswerRepository, AnswerRepository>();
-builder.Services.AddScoped<ICommentRepository, CommentRepository>();
-builder.Services.AddScoped<ILikedTemplateRepository, LikedTemplateRepository>();
-builder.Services.AddScoped<IStatisticRepository, StatisticRepository>();
+services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
 
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<ITemplateService, TemplateService>();
-builder.Services.AddScoped<IQuestionService, QuestionService>();
-builder.Services.AddScoped<IQuestionOptionService, QuestionOptionService>();
-builder.Services.AddScoped<IFormService, FormService>();
-builder.Services.AddScoped<IAnswerService, AnswerService>();
-builder.Services.AddScoped<ICommentService, CommentService>();
-builder.Services.AddScoped<ILikedTemplateService, LikedTemplateService>();
-builder.Services.AddScoped<IStatisticService, StatisticService>();
-
-builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
-
-builder.Services.AddScoped<JwtService>();
-builder.Services.Configure<AuthSettings>(builder.Configuration.GetSection("AuthSettings"));
-builder.Services.AddJwtTokens(builder.Configuration);
-
-builder.Services.AddSwaggerGen();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddControllers()
+services.AddControllers()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
     });
 
-builder.Services.AddCors(options =>
+services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173/")
+        policy.AllowAnyOrigin()
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
@@ -66,6 +33,9 @@ var app = builder.Build();
 app.UseRouting();
 
 app.UseCors("AllowFrontend");
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseSwagger();
 app.UseSwaggerUI();
