e20d6ed6b29c58f2586479f34237de97dd7eeaa9
KorotkovaKate <korotkovakate10@gmail.com>
2026-04-13 15:46:38 +0300

    feat (AnswerService): add Result pattern #1

--- Forms.Application/Interfaces/IServices/IAnswerService.cs
+++ Forms.Application/Interfaces/IServices/IAnswerService.cs
@@ -1,4 +1,5 @@
 using Forms.Application.DTOs.AnswerDTOs;
+using Forms.Core.Common;
 using Forms.Core.Models;
 
 namespace Forms.Application.Interfaces.IServices;
@@ -6,6 +7,6 @@ namespace Forms.Application.Interfaces.IServices;
 public interface 
     IAnswerService
 {
-    public Task<List<Answer>> GetAnswersByFormId(uint? formId);
-    public Task<List<Answer>> GetAnswersByQuestionId(uint? questionId);
+    public Task<Result<List<Answer>>> GetAnswersByFormId(uint? formId);
+    public Task<Result<List<Answer>>> GetAnswersByQuestionId(uint? questionId);
 }
--- Forms.Application/Services/AnswerService.cs
+++ Forms.Application/Services/AnswerService.cs
@@ -1,6 +1,8 @@
+using System.Net;
 using Forms.Application.DTOs.AnswerDTOs;
 using Forms.Application.Interfaces.IServices;
 using Forms.Application.Mapping;
+using Forms.Core.Common;
 using Forms.Core.Interfaces.IRepositories;
 using Forms.Core.Models;
 
@@ -8,19 +10,19 @@ namespace Forms.Application.Services;
 
 public class AnswerService(IAnswerRepository repository): IAnswerService
 {
-    public async Task<List<Answer>> GetAnswersByFormId(uint? formId)
+    public async Task<Result<List<Answer>>> GetAnswersByFormId(uint? formId)
     {
-        if (formId == null) {throw new ArgumentException("Incorrect form id");} 
+        if (formId == null) 
+            return Result<List<Answer>>.Failure("Incorrect form ID", HttpStatusCode.BadRequest);
         var answers = await repository.GetAnswersByFormId(formId.Value);
-        if(answers == null) throw new ArgumentException("No answers found");
-        return answers;
+        return Result<List<Answer>>.Success(answers);
     }
 
-    public async Task<List<Answer>> GetAnswersByQuestionId(uint? questionId)
+    public async Task<Result<List<Answer>>> GetAnswersByQuestionId(uint? questionId)
     {
-        if  (questionId == null) {throw new ArgumentException("Incorrect questionId");}
+        if  (questionId == null) 
+            return Result<List<Answer>>.Failure("Incorrect question ID", HttpStatusCode.BadRequest);
         var answers = await repository.GetAnswersByQuestionId(questionId.Value);
-        if(answers == null) throw new ArgumentException("No answers found");
-        return answers;
+        return Result<List<Answer>>.Success(answers);
     }
 }
