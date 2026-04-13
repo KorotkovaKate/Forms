569e6395279e888e2f63e84daf12cdf561d3fdd3
KorotkovaKate <korotkovakate10@gmail.com>
2026-04-13 15:18:13 +0300

    feat (IUserService): edit returning to Result #1

--- Forms.Application/Interfaces/IServices/IUserService.cs
+++ Forms.Application/Interfaces/IServices/IUserService.cs
@@ -1,14 +1,17 @@
 using Forms.Application.DTOs;
+using Forms.Core.Common;
 using Forms.Core.Models;
 
 namespace Forms.Application.Interfaces.IServices;
 
 public interface IUserService
 {
-    public Task<AuthorizationResponseDto> Authorize(AuthorizationDto authorizationDto);
+    public Task<Result<AuthorizationResponseDto>> Authorize(AuthorizationDto authorizationDto);
     public Task Registrate(RegistrationDto  registrationDto);
-    public Task<User> GetUserById(uint? userId);
-    public Task<List<GetAllUsersDto>> GetAllUsers();
-    public Task BlockUser(uint? userId);
-    public Task ActivateUser(uint? userId);
+    public Task<Result<User>> GetUserById(uint? userId);
+    public Task<Result<List<GetAllUsersDto>>> GetAllUsers();
+    public Task<Result<bool>> BlockUser(uint? userId);
+    public Task<Result<bool>> ActivateUser(uint? userId);
+    public Task<Result<bool>> AddToAdmin(uint? userId);
+    public Task<Result<bool>> RemoveFromAdmin(uint? userId);
 }
