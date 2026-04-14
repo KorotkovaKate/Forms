1d33858db77a5c2ee81f376522ccc3742562b664
KorotkovaKate <korotkovakate10@gmail.com>
2026-04-14 18:18:10 +0300

    feat (TemplateService): add result pattern #1

--- Forms.Application/Interfaces/IServices/ITemplateService.cs
+++ Forms.Application/Interfaces/IServices/ITemplateService.cs
@@ -1,16 +1,18 @@
 using Forms.Application.DTOs;
+using Forms.Core.Common;
 using Forms.Core.Models;
 
 namespace Forms.Application.Interfaces.IServices;
 
 public interface ITemplateService
 {
-    public Task CreateTemplate(CreateTemplateDto createTemplateDto);
-    public Task DeleteTemplate(uint? templateId);
-    public Task UpdateTemplate(UpdateTemplateDto  updateTemplateDto);
-    public Task<List<GetPublicTemplateDto>> GetAllPublicTemplates();
-    public Task<List<GetAllTemplatesDto>> GetAllTemplates();
-    public Task<Template> GetTemplateById(uint? templateId);
-    public Task IncreaseLikeNumber(uint? templateId);
-    public Task DecreaseLikeNumber(uint? templateId);
+    public Task<Result<bool>> CreateTemplate(CreateTemplateDto? createTemplateDto);
+    public Task<Result<bool>> DeleteTemplate(uint? templateId);
+    public Task<Result<bool>> UpdateTemplate(UpdateTemplateDto?  updateTemplateDto);
+    public Task<Result<List<GetPublicTemplateDto>>> GetAllPublicTemplates();
+    public Task<Result<List<GetAllTemplatesDto>>> GetAllTemplates();
+    public Task<Result<List<GetAllTemplatesDto>>> GetTemplatesByUserId(uint? userId);
+    public Task<Result<Template>> GetTemplateById(uint? templateId);
+    public Task<Result<bool>> IncreaseLikeNumber(uint? templateId);
+    public Task<Result<bool>> DecreaseLikeNumber(uint? templateId);
 }
