219d68232c22e04b5d058ecab3ea9d1bd04aa29f
KorotkovaKate <korotkovakate10@gmail.com>
2026-04-14 18:16:55 +0300

    feat (QuestionOptionService): add result pattern #1

--- Forms.Application/Services/QuestionOptionService.cs
+++ Forms.Application/Services/QuestionOptionService.cs
@@ -1,6 +1,8 @@
+using System.Net;
 using Forms.Application.DTOs.QuestionDTOs;
 using Forms.Application.Interfaces.IServices;
 using Forms.Application.Mapping;
+using Forms.Core.Common;
 using Forms.Core.Interfaces.IRepositories;
 using Forms.Core.Models;
 
@@ -8,36 +10,49 @@ namespace Forms.Application.Services;
 
 public class QuestionOptionService(IQuestionOptionRepository repository):IQuestionOptionService
 {
-    public async Task<List<QuestionOption>> GetOptionsByQuestionId(uint? questionId)
+    public async Task<Result<List<QuestionOption>>> GetOptionsByQuestionId(uint? questionId)
     {
-        if (questionId == null) {throw new ArgumentNullException("Incorrect question ID");}
+        if (questionId == null) {throw new ArgumentNullException("Incorrect question ID");} //flvalid
+        
         var options = await repository.GetOptionsByQuestionId(questionId.Value);
-        if (!options.Any()) throw new Exception("No options found");
-        return options;
+        if (options.Count == 0)
+            Result<List<QuestionOption>>.Failure("No options found", HttpStatusCode.InternalServerError);
+        
+        return Result<List<QuestionOption>>.Success(options);
     }
 
-    public async Task AddOption(AddOptionDto addOptionDto)
+    public async Task<Result<bool>> AddOption(AddOptionDto? addOptionDto)
     {
-        if (addOptionDto is null) throw new ArgumentNullException("Input data can't be null");
-        if (string.IsNullOrWhiteSpace(addOptionDto.Value)) throw new ArgumentException("Incorrect value");
+        if (addOptionDto is null) throw new ArgumentNullException("Input data can't be null"); //flvalid
+        if (string.IsNullOrWhiteSpace(addOptionDto.Value)) throw new ArgumentException("Incorrect value"); 
         if (addOptionDto.QuestionId == null) throw new ArgumentException("Incorrect question ID");
+        
         var questionOption = QuestionOptionMapping.AddOption(addOptionDto);
         await repository.AddOption(questionOption);
+        return Result<bool>.Success(true);
     }
 
-    public async Task DeleteOption(uint? questionOptionId)
+    public async Task<Result<bool>> DeleteOption(uint? questionOptionId)
     {
-        if (questionOptionId == null) throw new ArgumentNullException("Incorrect question ID");
-        var questionOption = await GetOptionById(questionOptionId);
-        if (questionOption == null) {throw new ArgumentNullException(nameof(questionOptionId));}
-        await repository.DeleteOption(questionOption);
+        if (questionOptionId == null) throw new ArgumentNullException("Incorrect question ID"); //flvalid
+        
+        var questionOptionResult = await GetOptionById(questionOptionId);
+        if (!questionOptionResult.IsSuccess) 
+            return Result<bool>.Failure(questionOptionResult.ErrorMessage, HttpStatusCode.NotFound);
+        
+        var response = questionOptionResult.Data;
+        await repository.DeleteOption(response);
+        return Result<bool>.Success(true);
     }
 
-    public async Task<QuestionOption> GetOptionById(uint? questionOptionId)
+    public async Task<Result<QuestionOption>> GetOptionById(uint? questionOptionId)
     {
-        if (questionOptionId == null) {throw new ArgumentNullException(nameof(questionOptionId));}
-        var questionOption = await GetOptionById(questionOptionId);
-        if (questionOption == null) {throw new ArgumentNullException("Question option not found");}
-        return questionOption;
+        if (questionOptionId == null) {throw new ArgumentNullException(nameof(questionOptionId));} //flvalid
+        
+        var questionOption = await repository.GetOptionById(questionOptionId.Value);
+        if (questionOption == null) 
+            return Result<QuestionOption>.Failure("Question option not found", HttpStatusCode.NotFound);
+        
+        return Result<QuestionOption>.Success(questionOption);
     }
 }
