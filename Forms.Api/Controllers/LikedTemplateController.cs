25b0aaf94bb04e1b92af76db3f5e31409f6d6b9f
KorotkovaKate <korotkovakate10@gmail.com>
2026-04-23 15:13:22 +0300

    feat (LikedTemplateController): add base controller #5

--- Forms.Api/Controllers/LikedTemplateController.cs
+++ Forms.Api/Controllers/LikedTemplateController.cs
@@ -7,47 +7,29 @@ using Microsoft.AspNetCore.Mvc;
 namespace Forms.Controllers;
 [ApiController]
 [Route("LikedTemplate")]
-public class LikedTemplateController(ILikedTemplateService service):ControllerBase
+public class LikedTemplateController(ILikedTemplateService service): BaseApiController
 {
     [HttpGet("GetLikedTemplates/{userId}")]
     public async Task<IActionResult> GetLikedTemplates([FromRoute] uint? userId)
     {
-        try
-        {
-            var templates = await service.GetLikedTemplates(userId);
-            return Ok(templates);
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        var response = await service.GetLikedTemplates(userId);
+        
+        return HandleResult(response);
     }
 
     [HttpPost("AddLikedTemplate")]
     public async Task<IActionResult> AddLikedTemplate([FromBody] LikedTemplateDto dto)
     {
-        try
-        {
-            await service.AddLikedTemplate(dto);
-            return Ok();
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        var response = await service.AddLikedTemplate(dto);
+        
+        return HandleResult(response);
     }
 
     [HttpPost("RemoveLikedTemplate")]
     public async Task<IActionResult> RemoveLikedTemplate([FromBody] LikedTemplateDto dto)
     {
-        try
-        {
-            await service.RemoveLikedTemplate(dto);
-            return Ok();
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        var response = await service.RemoveLikedTemplate(dto);
+        
+        return HandleResult(response);
     }
 }
