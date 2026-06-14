ffaf1b5fd77b9fab58887b69f00550ab4aee133f
KorotkovaKate <korotkovakate10@gmail.com>
2026-06-14 15:24:18 +0300

    feat (StatisticService): add calculate statistic method #17

--- Forms.Application/Interfaces/IServices/IStatisticService.cs
+++ Forms.Application/Interfaces/IServices/IStatisticService.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Forms.Application.DTOs;
+using Forms.Application.DTOs.StatisticDTOs;
 using Forms.Core.Common;
 using Forms.Core.Models;
 
@@ -9,6 +10,7 @@ namespace Forms.Application.Interfaces.IServices;
 public interface IStatisticService
 {
     public Task<Result<bool>> AddStatistic(uint? questionId);
-    public Task<Result<bool>> UpdateStatistic(UpdateStatisticDto updateStatisticDto);
+    public Task<Result<bool>> UpdateStatistic(UpdateStatisticDto? updateStatisticDto);
     public Task<Result<List<GetStatisticDto>>> GetStatisticsByTemplateId(uint? templateId);
+    public Task<Result<bool>> RecalculateTemplateStatistics(uint templateId);
 }
--- Forms.Application/Services/StatisticService.cs
+++ Forms.Application/Services/StatisticService.cs
@@ -4,26 +4,31 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Forms.Application.Common.Mapping;
+using Forms.Application.Common.Validators.StatisticValidators;
 using Forms.Application.DTOs;
+using Forms.Application.DTOs.StatisticDTOs;
 using Forms.Application.Interfaces.IServices;
 using Forms.Application.Mapping;
 using Forms.Core.Common;
+using Forms.Core.Exceptions;
 using Forms.Core.Interfaces.IRepositories;
 using Forms.Core.Models;
 
 namespace Forms.Application.Services;
 
-public  class StatisticService(IStatisticRepository statisticRepository, IAnswerService answerService, IQuestionService questionService):IStatisticService
+public  class StatisticService(IStatisticRepository statisticRepository, IAnswerService answerService, IQuestionService questionService): IStatisticService
 {
+    private const string FieldNullOrEmptyErrorMessage = "Field cannot be null or empty"; 
+    
     public async Task<Result<bool>> AddStatistic(uint? questionId)
     {
-        if(questionId == null) Result<bool>
-            .Failure("Incorrect question ID", HttpStatusCode.BadRequest);
+        if(questionId == null)
+            throw new ValidationException("questionId", FieldNullOrEmptyErrorMessage);
         
         var answersInResult = await answerService.GetAnswersByQuestionId(questionId.Value);
         if (!answersInResult.IsSuccess)
             return Result<bool>
-                .Failure(answersInResult.ErrorMessage, HttpStatusCode.InternalServerError); //can use my exception
+                .Failure(answersInResult.ErrorMessage, HttpStatusCode.InternalServerError);
         
         if (answersInResult.Data.Count == 0) 
             return Result<bool>
@@ -57,12 +62,15 @@ public  class StatisticService(IStatisticRepository statisticRepository, IAnswer
         return Result<bool>.Success(true);
     }
 
-    public async Task<Result<bool>> UpdateStatistic(UpdateStatisticDto updateStatisticDto)
+    public async Task<Result<bool>> UpdateStatistic(UpdateStatisticDto? updateStatisticDto)
     {
-        ArgumentNullException.ThrowIfNull(updateStatisticDto, "Update statistic Dto can't be null");
-        ArgumentNullException.ThrowIfNull(updateStatisticDto.StatisticId, "Update statistic Id can't be null");
-        ArgumentNullException.ThrowIfNull(updateStatisticDto.QuestionId, "QuestionId can't be null");
-        ArgumentNullException.ThrowIfNull(updateStatisticDto.TemplateId, "TemplateId can't be null");
+        if (updateStatisticDto == null)
+            throw new ValidationException("updateStatisticDto", FieldNullOrEmptyErrorMessage);
+        
+        var validator = new UpdateStatisticDtoValidator();
+        var validationResult = await validator.ValidateAsync(updateStatisticDto);
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.ToDictionary());
         
         var statistic = StatisticMapping.UpdateStatistic(updateStatisticDto);
         await statisticRepository.UpdateStatistic(statistic);
@@ -76,12 +84,63 @@ public  class StatisticService(IStatisticRepository statisticRepository, IAnswer
                 .Failure("Template Id can't be null", HttpStatusCode.BadRequest);
         
         var statisctic = await statisticRepository.GetStatisticsByTemplateId(templateId.Value);
-        if(!statisctic.Any()) 
+        if(statisctic.Count == 0) 
             return Result<List<GetStatisticDto>>
                 .Failure("No statistics found", HttpStatusCode.NotFound);
         
         var allStatistic = StatisticMapping.GetStatistic(statisctic);
         
         return Result<List<GetStatisticDto>>.Success(allStatistic);
+    }
+
+    public async Task<Result<bool>> RecalculateTemplateStatistics(uint templateId)
+    {
+        var existingStats = await statisticRepository.GetStatisticsByTemplateId(templateId);
+        var statsDictionary = existingStats.ToDictionary(s => s.QuestionId);
+        
+        var questionsResult = await questionService.GetQuestionsByTemplateId(templateId);
+        if (!questionsResult.IsSuccess)
+            return Result<bool>.Failure(questionsResult.ErrorMessage, HttpStatusCode.NotFound);
+
+        foreach (var question in questionsResult.Data)
+        { 
+            var answersInResult = await answerService.GetAnswersByQuestionId(question.Id); 
+            if (!answersInResult.IsSuccess || answersInResult.Data.Count == 0) 
+                continue;
+
+            var answers = answersInResult.Data;
+            
+            var grouped = answers
+                .GroupBy(a => a.Value)
+                .Select(g => new { Answer = g.Key, Count = g.Count() })
+                .OrderByDescending(g => g.Count)
+                .FirstOrDefault();
+
+            if (grouped is null) continue;
+            
+            int percent = (int)Math.Round((double)(grouped.Count * 100) / answers.Count);
+            
+            if (statsDictionary.TryGetValue(question.Id, out var existingStat))
+            {
+                existingStat.MostCommonAnswer = grouped.Answer;
+                existingStat.AnswerFrequencyInPercent = percent;
+                
+                await statisticRepository.UpdateStatistic(existingStat); 
+            }
+            else 
+            {
+                var newStatistic = new Statistic 
+                { 
+                    TemplateId = templateId, 
+                    QuestionId = question.Id, 
+                    MostCommonAnswer = grouped.Answer, 
+                    AnswerFrequencyInPercent = percent 
+                };
+            
+                await statisticRepository.AddStatistic(newStatistic);
+            }
+        }
+
+        return Result<bool>.Success(true);
     }
 }
