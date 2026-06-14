9390f74e5c13cd50d2c4fa6e6b6b5f5b4b612a94
KorotkovaKate <korotkovakate10@gmail.com>
2026-06-14 14:42:20 +0300

    feat (GetStatisticDto): add templateId and questionId #17

--- Forms.Application/DTOs/StatisticDTOs/GetStatisticDto.cs
+++ Forms.Application/DTOs/StatisticDTOs/GetStatisticDto.cs
@@ -5,7 +5,6 @@ public class GetStatisticDto
     public uint StatisticId { get; set; }
     public string MostCommonAnswer  { get; set; }
     public int AnswerFrequencyInPercent { get; set; }
-    public string TemplateTitle { get; set; }
-    public string? ImageUrl { get; set; }
-    public string QuestionTitle { get; set; }
+    public uint TemplateId { get; set; }
+    public uint QuestionId { get; set; }
 }
